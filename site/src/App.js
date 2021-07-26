61b248e5ef9de851c60dd5a015e31b220620b30c
Emilia Mahaney <emahaney@Emilias-Air.fios-router.home>
2021-07-26 11:06:24 -0400

    add pages and layout

--- site/src/App.js
+++ site/src/App.js
@@ -1,47 +1,40 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/home';
+import Two from './pages/two';
+import Three from './pages/three';
 
 export default function App() {
 	return (
 		<Router>
 			<div>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">1</Link>
-						</li>
-						<li>
-							<Link to="/two">2</Link>
-						</li>
-						<li>
-							<Link to="/three">3</Link>
-						</li>
-					</ul>
+				<nav style={{ background: 'gray', padding: '2em' }}>
+					<img src="https://picsum.photos/50" alt="" />
+					<Link to="/">Home</Link>
+
+					<Link to="/two">2</Link>
+
+					<Link to="/three">3</Link>
 				</nav>
-				<Switch>
-					<Route path="/two">
-						<Two />
-					</Route>
-					<Route path="/three">
-						<Three />
-					</Route>
-					<Route path="/">
-						<One />
-					</Route>
-				</Switch>
+				<div style={{ background: '#f9f9f9', minHeight: '30vh' }}>
+					<div style={{ padding: '2em' }}>
+						<Switch>
+							<Route path="/two">
+								<Two />
+							</Route>
+							<Route path="/three">
+								<Three />
+							</Route>
+							<Route path="/">
+								<Home />
+							</Route>
+						</Switch>
+					</div>
+				</div>
 			</div>
+			<footer style={{ padding: '2em', background: 'black', color: 'white' }}>
+				footer
+			</footer>
 		</Router>
 	);
-}
-
-function One() {
-	return <div>1</div>;
-}
-
-function Two() {
-	return <div>2</div>;
-}
-
-function Three() {
-	return <div>3</div>;
 }
